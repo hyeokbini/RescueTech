506d67c4b1780e33e8f39dad2644f7431ec4b514
고혁빈 <rhgurqls00@gmail.com>
2025-05-19 16:03:50 +0900

    [Test] UI 상호작용 테스트

---
+++ Assets/02.Scripts/TestButtonScript.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestButtonScript : MonoBehaviour
+{
+    public void Click()
+    {
+        Debug.Log("확인됨");;
+    }
+}
