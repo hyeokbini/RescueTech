7c232b7ee1b1dbe3d78dfb870adfd2a225c9f232
Rakhyunn <krh0323@pukyong.ac.kr>
2025-07-22 15:34:57 +0900

    [Feature] 화재 연습 모드 매니저 구현

---
+++ Assets/02.Scripts/FireScript/FirePracticeModeManager.cs
@@ -0,0 +1,69 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FirePracticeModeManager : MonoBehaviour, IManagerObjCount
+{
+    [SerializeField]
+    private int stageStepCount; // 스테이지에서 진행해야 할 총 단계
+    private int currentStepCount = 0; // 현재 단계
+    [SerializeField]
+    private GameObject clearUIPanel; // ui 오브젝트 연결
+    [SerializeField]
+    private TextUIManagerScript textManager;
+    [SerializeField]
+    private FireFadeController fireFadeController;
+    [SerializeField] 
+    private Transform player;
+    [SerializeField] 
+    private Transform respawnPoint;
+    public int ObjCount => currentStepCount;
+    [SerializeField]
+    private Towel towel;
+
+    void Start()
+    {
+        textManager.ActivateUIWithText();
+    }
+
+    // 각 오브젝트에 gamemanager를 참조해서 이 함수 실행
+    public void IncreaseStageStep()
+    {
+        currentStepCount++;
+        // 스테이지 클리어 로직 체크
+        if(currentStepCount == stageStepCount)
+        {
+            CheckStageClear();
+        }
+    }
+
+    // 스테이지 클리어 로직
+    private void CheckStageClear()
+    {
+        StartCoroutine(ClearFade());
+        if (clearUIPanel != null)
+        {
+            clearUIPanel.SetActive(true);
+        }
+        else
+        {
+            Debug.Log("UI 할당되지 않음");
+        }
+    }
+
+    private IEnumerator ClearFade()
+    {
+        // 1. 페이드 아웃 (화면 검게)
+        fireFadeController.FadeOut();
+        // 페이드 완료될 때까지 대기
+        while (fireFadeController.IsFading) yield return null;
+
+        // 2. 플레이어 위치 이동
+        if (player != null && respawnPoint != null)
+            player.position = respawnPoint.position;
+        towel.PutOffFace();
+        // 3. 페이드 인 (원래 화면)
+        fireFadeController.FadeIn();
+        while (fireFadeController.IsFading) yield return null;
+    }
+}
