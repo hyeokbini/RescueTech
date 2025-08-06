ddd68c43932e01a4778807c7d9960ab56552ba59
Rakhyunn <krh0323@pukyong.ac.kr>
2025-08-06 19:39:58 +0900

    [Feature] 실전모드용 매니저 구현, 상호작용 스크립트 실전모드 조건 추가
    -실전모드애서 사용할 매니저를 구현
    -연습모드에서 사용한 상호작용 스크립트에 실전모드 조건 추가

---
+++ Assets/02.Scripts/FireScript/FireModeSetManager.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FireModeSetManager : MonoBehaviour
+{
+    // 연습모드, 실전모드 오브젝트 그룹의 부모 오브젝트 참조
+    [SerializeField] private GameObject realModeObjGroup;
+    [SerializeField] private GameObject practiceModeObjGroup;
+    void Start()
+    {
+        if(ModeManagerScript.Instance.isRealMode)
+        {
+            realModeObjGroup.SetActive(true);
+            practiceModeObjGroup.SetActive(false);
+        }
+        else
+        {
+            realModeObjGroup.SetActive(false);
+            practiceModeObjGroup.SetActive(true);
+        }
+    }
+}
