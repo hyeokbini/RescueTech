52350501671f596050fb3bcc7c14aec3257ce761
jeongha-daisy <jeongha.daisy@gmail.com>
2025-09-23 21:35:44 +0900

    [Feature] 화살표 오브젝트 모드 전환

--- Assets/02.Scripts/IndustryFireScript/IFSceneModeInitializer.cs
+++ Assets/02.Scripts/IndustryFireScript/IFSceneModeInitializer.cs
@@ -8,8 +8,7 @@ public class IFSceneModeInitializer : MonoBehaviour
     [SerializeField] private GameObject realManager;
 
     // 상호 작용하는 오브젝트를 구분한다. 
-    [SerializeField] private GameObject practiceObjects;
-    [SerializeField] private GameObject realObjects;
+    [SerializeField] private GameObject directionObjects;
 
 
     private bool isRealMode;
@@ -29,12 +28,14 @@ public class IFSceneModeInitializer : MonoBehaviour
             {
                 Debug.Log("연습 모드 실행");
                 practiceManager.SetActive(true);
+                directionObjects.SetActive(true);
                 realManager.SetActive(false);
             }
             else
             {
                 Debug.Log("실전 모드 실행");
                 practiceManager.SetActive(false);
+                directionObjects.SetActive(false);
                 realManager.SetActive(true);
             }
         }
