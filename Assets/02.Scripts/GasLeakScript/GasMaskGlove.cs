460a154138797db32f2c783f85fd3d1cf4e306de
Rakhyunn <krh0323@pukyong.ac.kr>
2025-11-01 15:16:03 +0900

    [Feature] 방독면, 장갑 상호작용 시 동작 구현

--- Assets/02.Scripts/GasLeakScript/GasMaskGlove.cs
+++ Assets/02.Scripts/GasLeakScript/GasMaskGlove.cs
@@ -13,10 +13,54 @@ public class GasMaskGlove : MonoBehaviour, IInteractable
     private GasWearType gasWearType;
     [SerializeField]
     GasWear gasWear;
+    [SerializeField]
+    private GameObject leftGlove;
+    [SerializeField]
+    private GameObject rightGlove;
+    [SerializeField]
+    private GameObject leftController;
+    [SerializeField]
+    private GameObject rightController;
+    [SerializeField]
+    private GameObject gasMask;
     public void Wear()
     {
         if (hasInteracted) return;
         hasInteracted = true;
+        if (gasWearType == GasWearType.Glove)
+        {
+            if (leftGlove != null && leftController != null)
+            {
+                leftGlove.transform.SetParent(leftController.transform);
+                leftGlove.transform.localPosition = Vector3.zero;
+                leftGlove.transform.localRotation = Quaternion.Euler(0f, 180f, 0f);
+
+                // 하위에서 "Model" 오브젝트 찾아서 비활성화
+                Transform leftModel = leftController.transform.Find("Model");
+                if (leftModel != null)
+                {
+                    leftModel.gameObject.SetActive(false);
+                }
+            }
+
+            if (rightGlove != null && rightController != null)
+            {
+                rightGlove.transform.SetParent(rightController.transform);
+                rightGlove.transform.localPosition = Vector3.zero;
+                rightGlove.transform.localRotation = Quaternion.Euler(0f, 180f, 0f);
+
+                // 하위에서 "Model" 오브젝트 찾아서 비활성화
+                Transform rightModel = rightController.transform.Find("Model");
+                if (rightModel != null)
+                {
+                    rightModel.gameObject.SetActive(false);
+                }
+            }
+        }
+        else if (gasWearType == GasWearType.Mask)
+        {
+            if (gasMask) gasMask.SetActive(false);
+        }
         gasWear.Check(gasWearType);
     }
 }
